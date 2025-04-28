da89fd1ceb556aca26bba9a5fd48da5e090bfdab
Suhaib Ahmad <suhaibraza42@gmail.com>
2025-04-28 18:24:12 +0500

    timeline model, controller, routes implemented

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import dbConnection from "./database/DbConnection.js";
 import { errorMiddleware } from "./middlewares/error.js";
 import messageRouter from "./router/messageRoutes.js";
 import userRouter from "./router/userRoutes.js";
+import timelineRouter from "./router/timelineRoutes.js";
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(
 );
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/timeline", timelineRouter);
 
 dbConnection();
 app.use(errorMiddleware);
---
+++ backend/controller/timelineController.js
@@ -0,0 +1,47 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import errorHandler from "../middlewares/error.js";
+import { Timeline } from "../models/timelineSchema.js";
+
+export const postTimeline = catchAsyncErrors(async (req, res, next) => {
+  const { title, description, from, to } = req.body;
+  const timeline = await Timeline.create({
+    title,
+    description,
+    timeline: {
+      from,
+      to,
+    },
+  });
+  if (!timeline) {
+    return next(new errorHandler("Timeline not created", 400));
+  }
+  res.status(201).json({
+    success: true,
+    timeline,
+  });
+});
+
+export const getAllTimeline = catchAsyncErrors(async (req, res, next) => {
+  const timelines = await Timeline.find();
+  if (!timelines) {
+    return next(new errorHandler("No timelines found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    timelines,
+  });
+});
+
+export const deleteTimeline = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const timeline = await Timeline.findByIdAndDelete(id);
+  if (!timeline) {
+    return next(new errorHandler("Timeline not found", 404));
+  }
+  await timeline.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Timeline deleted successfully",
+  });
+});
